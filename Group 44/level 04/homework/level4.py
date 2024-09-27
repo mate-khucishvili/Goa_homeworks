43d51caea4bcc546239b0bf30e1d90af39208817
mate-khucishvili <materobloxs555@gmail.com>
2024-09-27 22:04:49 +0400

    second commit

---
+++ Group 44/level 04/homework/level4.py
@@ -0,0 +1,29 @@
+name = input("please enter your name:  ")
+surname = "qamatadze"
+print(name + " " + surname)
+
+age = input("please enter your age:  ")
+print(int(age) + int(10))
+
+num1 = input("please enter number:  ")
+num2 = input("please enter number:  ")
+num3 = input("please enter number:  ")
+num4 = input("please enter number:  ")
+num5 = input("please enter number:  ")
+
+print(int(num1) + int(num2) + int(num3) + int(num4) + int(num5) / 5)
+
+#debugging არის შეცდომების აღმოფხვრა
+#Case Sensitive არის ერთი და იგივე აზრის სიტყვა ოღონდ განსხვავდებიან რეგისტრით და კომპიუტერი აღიქვამს როგორც ახალ ცვლადს მაგალითად name, Name, NAME,
+#Snake Case როცა შენ ქმნი დიდ ცვლადს რომელშიც დაგჭირდება რამდენიმე სიტყვა და Snake Case ამ ცვლადის ამოკითხვას გაგიმარტივებს ( _ ) დაგეხმარება უფრო მარტივ ამოკითხვაში 
+#Snake Case და მაგალითად My_dog_name 
+
+move = "Jurassic World Dominion"
+Director = "Colin Trevorrow" #აქ მინიჭების ოპერატორი უნდა იყოს - მაგივრად
+Year = 2022 #აქაც მინიჭების ოპერატორი უნდოდა - მაგივრად
+actors = "Jeff Goldblum" #აქ აკლდა " " ბრჭყალები
+actors  = "Sam Neill" #აქ მინიჭების ოპერატორი აკლდა და ასევე აქ Sam Neill აკლდა " "
+actors = "Chris Pratt" #აქ მინიჭების ოპერატორი აკლდა  და Chris Pratt ჩავსვი " "
+
+
+
