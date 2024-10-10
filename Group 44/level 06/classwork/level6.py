416a7beb2a4b1b1f20f59845828dd659cf01f58f
mate-khucishvili <materobloxs555@gmail.com>
2024-10-10 22:20:26 +0400

    second commit

---
+++ Group 44/level 06/classwork/level6.py
@@ -0,0 +1,15 @@
+age = input("please enter your age:  ")
+print(int(age) / int(10))
+
+age1 = int(input("please enter your age:  "))
+print(type(age))
+
+age2 = float(age1)
+print(type(age2))
+
+
+
+
+name = input("please enter your name:  ")
+age = input("please enter number:  ")
+print(name * int(age))                                #implicit - ავტომატურად მონაცემთა ტიპის შეცვლა ხოლო explicit არის როცა ჩვენ მონაცემთა ტიპის შევცვლით ჩვენ თვითონ
---
+++ Group 44/level 06/homework/level6.py
@@ -0,0 +1,41 @@
+age = input(("enter your age:  "))
+print(int(age) + int(10))
+print(type(float(age)))
+
+name = input(("enter your surname:  "))
+print(name * 100)
+
+a = "Sam"
+b = "sung"
+print(a + b)
+
+a = "pro"
+b = "gram"
+print(a + b)
+
+a = "sch"
+b = "ool"
+print(a + b)
+
+a = "Eng"
+b = "lish"
+print(a + b)
+
+a = "cin"
+b = "ema"
+print(a + b)
+
+#პროგრამირებაში არსებობს სხვადასხვა მონაცემთა ტიპები მაგალითად წილადი რიცხვები,მთელი რიცხვები და ნებისმიერი მონაცემი მოქცეული ბრჭტალებიში.
+#და რომ როგორმე ერთმანეთისგან განვავასხვავოთ ერთმანეთისგან
+
+num1 = 300
+print(type(num1))
+
+num2 = 5.3
+print(type(num2))
+
+num3 = "Mate"
+print(type(num3))
+
+num4 = -444
+print(type(num4))
